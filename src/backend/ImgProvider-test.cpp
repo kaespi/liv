2b1c1bf33d9b1884309f04bb288cfbddf056aa3b
Kaspar Giger <kg@kgmw.ch>
2023-03-31 22:44:07 +0200

    more unittests for class ImgProvider

--- src/backend/ImgProvider-test.cpp
+++ src/backend/ImgProvider-test.cpp
@@ -18,10 +18,29 @@ class ImgProviderTest : public ::testing::Test
     QSize size;
 };
 
-TEST_F(ImgProviderTest, WHEN_image_without_underscore_is_requested_THEN_current_file_is_loaded)
+TEST_F(ImgProviderTest,
+       GIVEN_ImgProvider_WHEN_image_without_underscore_is_requested_THEN_current_file_is_loaded)
 {
     MockFileSystemWalker fileSystemWalker;
     ImgProvider imgProvider(&fileSystemWalker);
     EXPECT_CALL(fileSystemWalker, getCurrentFile()).Times(::testing::Exactly(1));
     imgProvider.requestPixmap("", &size, size);
+}
+
+TEST_F(ImgProviderTest,
+       GIVEN_ImgProvider_WHEN_image_with_underscore_next_is_requested_THEN_next_file_is_loaded)
+{
+    MockFileSystemWalker fileSystemWalker;
+    ImgProvider imgProvider(&fileSystemWalker);
+    EXPECT_CALL(fileSystemWalker, getNextFile()).Times(::testing::Exactly(1));
+    imgProvider.requestPixmap("test_next", &size, size);
+}
+
+TEST_F(ImgProviderTest,
+       GIVEN_ImgProvider_WHEN_image_with_underscore_prev_is_requested_THEN_prev_file_is_loaded)
+{
+    MockFileSystemWalker fileSystemWalker;
+    ImgProvider imgProvider(&fileSystemWalker);
+    EXPECT_CALL(fileSystemWalker, getPrevFile()).Times(::testing::Exactly(1));
+    imgProvider.requestPixmap("sometext42_prev", &size, size);
 }
