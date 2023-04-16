a1d1e6136ca3360daa886ec47c1302d1eaec9c3f
Kaspar Giger <kg@kgmw.ch>
2023-04-16 22:57:09 +0200

    more unittests for class FileSystemWalker

--- src/backend/FileSystemWalker-test.cpp
+++ src/backend/FileSystemWalker-test.cpp
@@ -6,8 +6,10 @@
 #include <gtest/gtest.h>
 #include <memory>
 
+using ::testing::_; // NOLINT
 using ::testing::Eq;
 using ::testing::NiceMock;
+using ::testing::Return;
 
 class MockDir : public IDir
 {
@@ -47,4 +49,25 @@ TEST_F(
     EXPECT_THAT(fileSystemWalker.getCurrentFile(), Eq(QString{""}));
     EXPECT_THAT(fileSystemWalker.getNextFile(), Eq(QString{""}));
     EXPECT_THAT(fileSystemWalker.getPrevFile(), Eq(QString{""}));
+}
+
+TEST_F(
+    FileSystemWalkerTest,
+    GIVEN_FileSystemWalker_is_initialized_with_existing_file_WHEN_getting_file_THEN_the_same_file_is_always_returned)
+{
+    const QString someFolder{"/xyz"};
+    const QString existingFile{"a.jpg"};
+    const QString fullPathExistingFile{someFolder + "/" + existingFile};
+
+    FileSystemMockFactory fsFactoryMock;
+    QList<QByteArray> fileTypes{};
+    EXPECT_CALL(*fsFactoryMock.m_ptrDir, entryList(_, _, _))
+        .WillRepeatedly(Return(QStringList{existingFile}));
+    EXPECT_CALL(*fsFactoryMock.m_ptrDir, absolutePath()).WillRepeatedly(Return(someFolder));
+
+    FileSystemWalker fileSystemWalker(existingFile, &fsFactoryMock, fileTypes);
+    EXPECT_THAT(fileSystemWalker.getCurrentFile(), Eq(fullPathExistingFile));
+    EXPECT_THAT(fileSystemWalker.getNextFile(), Eq(fullPathExistingFile));
+    EXPECT_THAT(fileSystemWalker.getNextFile(), Eq(fullPathExistingFile));
+    EXPECT_THAT(fileSystemWalker.getPrevFile(), Eq(fullPathExistingFile));
 }
