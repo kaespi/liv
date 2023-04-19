894032b6d633a2f33b2687600dc74f3f0f72ca8d
Kaspar Giger <kg@kgmw.ch>
2023-04-19 21:24:29 +0200

    less repeated code in FileSystemWalker unittests

--- src/backend/FileSystemWalker-test.cpp
+++ src/backend/FileSystemWalker-test.cpp
@@ -51,16 +51,16 @@ class FileSystemMockFactory : public IFileSystemClassesFactory
 class FileSystemWalkerTest : public ::testing::Test
 {
   protected:
+    FileSystemMockFactory m_fsFactoryMock;
+    QList<QByteArray> m_fileTypes{};
 };
 
 TEST_F(
     FileSystemWalkerTest,
     GIVEN_FileSystemWalker_is_initialized_with_inexistent_file_WHEN_getting_file_THEN_an_empty_string_is_returned)
 {
-    FileSystemMockFactory fsFactoryMock;
-    QList<QByteArray> fileTypes{};
-    FileSystemWalker fileSystemWalker("inexistent_file.jpg", &fsFactoryMock, fileTypes);
-    EXPECT_CALL(*fsFactoryMock.m_ptrFileSystem, fileExists(_)).WillRepeatedly(Return(false));
+    FileSystemWalker fileSystemWalker("inexistent_file.jpg", &m_fsFactoryMock, m_fileTypes);
+    EXPECT_CALL(*m_fsFactoryMock.m_ptrFileSystem, fileExists(_)).WillRepeatedly(Return(false));
     EXPECT_THAT(fileSystemWalker.getCurrentFile(), Eq(QString{""}));
     EXPECT_THAT(fileSystemWalker.getNextFile(), Eq(QString{""}));
     EXPECT_THAT(fileSystemWalker.getPrevFile(), Eq(QString{""}));
@@ -74,14 +74,12 @@ TEST_F(
     const QString existingFile{"a.jpg"};
     const QString fullPathExistingFile{someFolder + "/" + existingFile};
 
-    FileSystemMockFactory fsFactoryMock;
-    QList<QByteArray> fileTypes{};
-    EXPECT_CALL(*fsFactoryMock.m_ptrDir, entryList(_, _, _))
+    EXPECT_CALL(*m_fsFactoryMock.m_ptrDir, entryList(_, _, _))
         .WillRepeatedly(Return(QStringList{existingFile}));
-    EXPECT_CALL(*fsFactoryMock.m_ptrDir, absolutePath()).WillRepeatedly(Return(someFolder));
-    EXPECT_CALL(*fsFactoryMock.m_ptrFileSystem, fileExists(_)).WillRepeatedly(Return(true));
+    EXPECT_CALL(*m_fsFactoryMock.m_ptrDir, absolutePath()).WillRepeatedly(Return(someFolder));
+    EXPECT_CALL(*m_fsFactoryMock.m_ptrFileSystem, fileExists(_)).WillRepeatedly(Return(true));
 
-    FileSystemWalker fileSystemWalker(existingFile, &fsFactoryMock, fileTypes);
+    FileSystemWalker fileSystemWalker(existingFile, &m_fsFactoryMock, m_fileTypes);
     EXPECT_THAT(fileSystemWalker.getCurrentFile(), Eq(fullPathExistingFile));
     EXPECT_THAT(fileSystemWalker.getNextFile(), Eq(fullPathExistingFile));
     EXPECT_THAT(fileSystemWalker.getNextFile(), Eq(fullPathExistingFile));
@@ -91,13 +89,11 @@ TEST_F(
 TEST_F(FileSystemWalkerTest,
        GIVEN_browsing_folder_with_multiple_files_WHEN_getting_next_file_THEN_the_files_are_returned_in_order)
 {
-    FileSystemMockFactory fsFactoryMock;
-    QList<QByteArray> fileTypes{};
-    EXPECT_CALL(*fsFactoryMock.m_ptrDir, entryList(_, _, _))
+    EXPECT_CALL(*m_fsFactoryMock.m_ptrDir, entryList(_, _, _))
         .WillRepeatedly(Return(QStringList{"a.jpg", "b.jpg", "c.jpg"}));
-    EXPECT_CALL(*fsFactoryMock.m_ptrFileSystem, fileExists(_)).WillRepeatedly(Return(true));
+    EXPECT_CALL(*m_fsFactoryMock.m_ptrFileSystem, fileExists(_)).WillRepeatedly(Return(true));
 
-    FileSystemWalker fileSystemWalker("a.jpg", &fsFactoryMock, fileTypes);
+    FileSystemWalker fileSystemWalker("a.jpg", &m_fsFactoryMock, m_fileTypes);
     EXPECT_THAT(fileSystemWalker.getCurrentFile(), Eq("/a.jpg"));
     EXPECT_THAT(fileSystemWalker.getNextFile(), Eq("/b.jpg"));
     EXPECT_THAT(fileSystemWalker.getCurrentFile(), Eq("/b.jpg"));
@@ -109,13 +105,11 @@ TEST_F(
     FileSystemWalkerTest,
     GIVEN_browsing_folder_with_multiple_files_WHEN_getting_previous_file_THEN_the_files_are_returned_in_order)
 {
-    FileSystemMockFactory fsFactoryMock;
-    QList<QByteArray> fileTypes{};
-    EXPECT_CALL(*fsFactoryMock.m_ptrDir, entryList(_, _, _))
+    EXPECT_CALL(*m_fsFactoryMock.m_ptrDir, entryList(_, _, _))
         .WillRepeatedly(Return(QStringList{"a.jpg", "b.jpg", "c.jpg"}));
-    EXPECT_CALL(*fsFactoryMock.m_ptrFileSystem, fileExists(_)).WillRepeatedly(Return(true));
+    EXPECT_CALL(*m_fsFactoryMock.m_ptrFileSystem, fileExists(_)).WillRepeatedly(Return(true));
 
-    FileSystemWalker fileSystemWalker("a.jpg", &fsFactoryMock, fileTypes);
+    FileSystemWalker fileSystemWalker("a.jpg", &m_fsFactoryMock, m_fileTypes);
     EXPECT_THAT(fileSystemWalker.getCurrentFile(), Eq("/a.jpg"));
     EXPECT_THAT(fileSystemWalker.getPrevFile(), Eq("/c.jpg"));
     EXPECT_THAT(fileSystemWalker.getPrevFile(), Eq("/b.jpg"));
